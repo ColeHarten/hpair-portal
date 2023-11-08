ea6eb35fce371462746151c97b73a457323430ae
Cole Harten <charten@college.harvard.edu>
2023-11-07 22:23:54 -0500

    added logos

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,9 +1,9 @@
-import { Typography, AppBar as MuiAppBar, Toolbar } from '@mui/material';
+import { Box, AppBar as MuiAppBar, Toolbar } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import AccountMenu from "./AccountMenu";
-import { styled } from '@mui/material/styles';
 import ExternalLink from './ExternalLink';
-
 
 export default function MenuBar({ user, isSignedIn, isInConf, onMenuButtonClick }) {
   const CustomAppBar = styled(MuiAppBar)(({ theme }) => ({
@@ -13,30 +13,24 @@ export default function MenuBar({ user, isSignedIn, isInConf, onMenuButtonClick 
   return (
     <CustomAppBar position="fixed">
       <Toolbar>
-        <Typography
-          component={Link}
-          to="https://www.hpair.org"
-          variant="h6"
-          color="inherit"
-          noWrap
-          sx={{ flexGrow: 1 }}
-          style={{ textDecoration: 'none' }}
-        >
-          HPAIR
-        </Typography>
-        {!isSignedIn ? (
-          <>
-            <ExternalLink component={Link} to="https://www.hpair.org" color="inherit" style={{ textDecoration: 'none' }}>
-              Home
+        <Box display="flex" sx={{justifyContent: 'space-between'}}>
+            <ExternalLink component={Link} to="https://www.hpair.org">
+              <img src="/HPAIR Logo Banner (White).png" alt="HPAIR Logo" width={300} />
             </ExternalLink>
-            <ExternalLink component={Link} to="https://www.hpair.org/about-us-1" color="inherit" style={{ textDecoration: 'none' }}>
-              About Us
-            </ExternalLink>
-            <ExternalLink to="https://www.hpair.org/board-of-advisors">Board of Advisors</ExternalLink>
-          </>
-        ) : 
-          <AccountMenu user={user} onMenuButtonClick={onMenuButtonClick} />
-        }
+            {!isSignedIn ? (
+              <>
+                <ExternalLink component={Link} to="https://www.hpair.org" color="inherit" style={{ textDecoration: 'none' }}>
+                  Home
+                </ExternalLink>
+                <ExternalLink component={Link} to="https://www.hpair.org/about-us-1" color="inherit" style={{ textDecoration: 'none' }}>
+                  About Us
+                </ExternalLink>
+                <ExternalLink to="https://www.hpair.org/board-of-advisors">Board of Advisors</ExternalLink>
+              </>
+            ) : 
+              <AccountMenu user={user} onMenuButtonClick={onMenuButtonClick} />
+            }
+        </Box>
       </Toolbar>
     </CustomAppBar>
   );
