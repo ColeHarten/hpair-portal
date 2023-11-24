237b30aadb145f852e4da138d2caeddac208c0c8
Cole Harten <charten@college.harvard.edu>
2023-11-23 21:24:41 -0500

    added varible price support

--- src/components/home/PaymentWidget.js
+++ src/components/home/PaymentWidget.js
@@ -7,8 +7,9 @@ export default function PaymentWidget({user, joinCode}){
      // creates a paypal order
     const createOrder = async (data, actions) => {
         // get the price of the conference
-        const confData = await getConferenceData(joinCode.slice(0,6));
-        const price = confData.price;
+        const confData = await getConferenceData(joinCode.slice(0,7));
+        // access the element 'F' from the prices map
+        const price = confData.prices[joinCode.split('-')[1]];
 
         return actions.order.create({
             purchase_units: [
@@ -33,7 +34,7 @@ export default function PaymentWidget({user, joinCode}){
         return actions.order.capture().then(async (details) => {
             const { payer } = details;
             // add user to conference
-            await addConferenceCode(user, joinCode.slice(0,6));
+            await addConferenceCode(user, joinCode);
             // add payment info to payments collection
             await addPaymentInfo(user, {
                 amount: details.purchase_units[0].amount.value,
