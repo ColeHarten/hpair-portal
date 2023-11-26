bf6b94c4bd37f3738e704d2a1e2e40ff88ed988f
Cole Harten <charten@college.harvard.edu>
2023-11-26 14:48:44 -0500

    ...

--- src/components/confPage/MerchStore.js
+++ src/components/confPage/MerchStore.js
@@ -1,11 +1,12 @@
 // Function to hold the mech store page
 import { Box, AppBar, Toolbar, Button, Typography } from '@mui/material';
+import StoreWidget from './StoreWidget';
 import React from 'react';
 
 export default function MerchStore({user}) {
     return (
         <Box>
-            <Typography> Merch Store </Typography>
+            <StoreWidget />
         </Box>
     )
 }
