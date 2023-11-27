e8d2eebeaae642350bd70aaca6218545ac3a394e
Cole Harten <charten@college.harvard.edu>
2023-11-27 13:09:57 -0500

    added heading

--- src/components/confPage/ConfPage.js
+++ src/components/confPage/ConfPage.js
@@ -1,13 +1,24 @@
 import { Box, Typography } from "@mui/material";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
+import { getUserData } from "../../utils/mutations";
 
 export default function ConfPage({ user }) {
   const { confCode } = useParams();
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const getConfData = async () => {
+      const confData = await getUserData(user);
+      setName(confData.displayName);
+    }
+    getConfData();
+  }, [confCode]);
 
   return (
-    <Box>
-      <Typography> {confCode} </Typography>
-    </Box>
+<Box sx={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+  <Typography variant="h4"> Congratulations, {name.split(' ')[0]}! </Typography>
+</Box>
+
   );
 }
