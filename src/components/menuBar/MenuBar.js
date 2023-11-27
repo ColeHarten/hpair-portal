e93d611d007dca1011a1949f42b01bed40d5785c
Cole Harten <charten@college.harvard.edu>
2023-11-27 12:50:47 -0500

    added more spacing

--- src/components/menuBar/MenuBar.js
+++ src/components/menuBar/MenuBar.js
@@ -41,7 +41,7 @@ export default function MenuBar({ user, onMenuButtonClick }) {
         <Button onClick={() => { navigate(`/${confCode}`); }}>
           <img src="/art/HPAIR Logo Banner (White).png" alt="HPAIR Logo" width={300} />
         </Button>
-        <Box sx={{display: 'flex'}}>
+        <Box sx={{display: 'flex', gap: '10px'}}>
           <Button color="inherit" style={{ textDecoration: 'none' }} onClick={() => { navigate(`/${confCode}/store`); }}>Store</Button>
           <AccountMenu user={user} onMenuButtonClick={onMenuButtonClick} />
         </Box>
