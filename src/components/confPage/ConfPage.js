a446892fcae31f5775c4f4b5c7ea77ed133af721
Cole Harten <charten@college.harvard.edu>
2023-12-07 16:54:31 -0500

    added confetti!

--- src/components/confPage/ConfPage.js
+++ src/components/confPage/ConfPage.js
@@ -2,10 +2,12 @@ import { Box, Typography } from "@mui/material";
 import React, {useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
 import { getUserData } from "../../utils/mutations";
+import Confetti from 'react-dom-confetti';
 
 export default function ConfPage({ user }) {
   const { confCode } = useParams();
   const [name, setName] = useState('');
+  const [confetti, setConfetti] = useState(false);
 
   useEffect(() => {
     const getConfData = async () => {
@@ -13,10 +15,25 @@ export default function ConfPage({ user }) {
       setName(confData?.displayName);
     }
     getConfData();
+    setConfetti(true);
   }, [user, confCode]);
+
+  const confettiConfig = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 30,
+    elementCount: 200,
+    dragFriction: 0.10,
+    duration: 8000,
+    stagger: 3,
+    width: "10px",
+    height: "10px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+  };
 
   return (
   <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', padding: '20px' }}>
+    <Confetti active={confetti} config={confettiConfig} />
     <Typography variant="h4" sx={{ mb: 2 }}>Congratulations, {name?.split(' ')[0]}!</Typography>
     <Typography variant="body1" sx={{ mb: 2 }}>We can't wait to see you in February!</Typography>
     <iframe title="welcome video" style={{ width: '50vw', height: '60vh' }} src="https://www.youtube.com/embed/omH9elhUtKI?autoplay=1&mute=0">
